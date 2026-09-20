a59ad9a59a448b28fd36a882edd24e80fbd59f0b
Fernando Pereira <fernandopereirajuliano009@gmail.com>
2026-09-20 18:40:26 -0300

    Criado Post e Get

---
+++ primeiroSpring/Controller/ProdutoController.java
@@ -0,0 +1,33 @@
+package com.example.primeiroSpring.Controller;
+
+import com.example.primeiroSpring.Model.ProdutoDTO;
+import com.example.primeiroSpring.Model.ProdutoEntidade;
+import com.example.primeiroSpring.Servico.ProdutoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+// DEVEMOS SEMPRE ANOTAR OQ A CLASSE É
+@RestController
+@RequestMapping("/v1/produtos")
+@RequiredArgsConstructor
+public class ProdutoController {
+
+private final ProdutoService produtoService;
+
+@GetMapping
+@ResponseStatus(HttpStatus.OK)
+    public List<ProdutoEntidade> getAll(){
+    return produtoService.getAll();
+}
+
+@PostMapping
+@ResponseStatus(HttpStatus.CREATED)
+public ProdutoEntidade post (@RequestBody ProdutoDTO produtoDTO){
+    return produtoService.post(produtoDTO);
+}
+
+
+}
