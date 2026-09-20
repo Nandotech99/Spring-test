1aab03f53b68b98c1dd8b29d14c35fc3c9a45860
Nandotech99 <fernandopereirajuliano009@gmail.com>
2026-09-20 20:29:05 -0300

    Terminei

---
+++ primeiro-Springboot/src/main/java/com/example/primeiro_Springboot/PrimeiroSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.primeiro_Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroSpringbootApplication.class, args);
+	}
+
+}
---
+++ primeiro-Springboot/src/main/java/com/example/primeiro_Springboot/Servico/HelloWorldService.java
@@ -0,0 +1,15 @@
+package com.example.primeiro_Springboot.Servico;
+
+import org.springframework.stereotype.Service;
+
+// ESTA CLASSE É ONDE FIACA AS LOGICAS  DA REGRA DE NEGOCIO DA APLICAÇÃO
+//O CONTROLER RECEBE A REQUISIÇÃO E PASSA PARA A CLASSE SERVECE PARA PROCESSAR
+
+
+@Service // INDICA QUE ESTA CLASSE É UM SERVICO
+public class HelloWorldService {
+
+    public String hello(String name){
+        return "Eae, "+ name+"! Tudo bem?";
+    }
+}
